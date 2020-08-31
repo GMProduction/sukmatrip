32060b38637eca05249b81d6f8e706c6e16cb2ee
pradana mahendra <pradanamahendra@gmail.com>
2020-09-01 00:43:46 +0700

    push

---
+++ resources/views/base.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Sukmatrip honeymoon trip for couple.">
+    <meta name="author" content="Genossys">
+    <title>Sukmatrip - Honeymoon & Trip</title>
+
+    <link rel="stylesheet" href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" type="text/css">
+    <!-- Favicon -->
+{{--    <link rel="icon" href="../assets/img/brand/favicon.png" type="image/png">--}}
+<!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
+</head>
+
+<body style="background-color: #eeeeee">
+<!-- Main content -->
+<div class="main-content" id="panel">
+    <!-- As a link -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white" style="border-bottom: 3px solid #dddddd">
+        <a class="navbar-brand text-xl text-primary" href="#" style=" font-weight: bold">Sistem Informasi Permohonan Jual Beli</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto">
+                <a class="nav-item nav-link text-sm ml-3 active" href="/">Beranda <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link text-sm ml-3" href="/kontak">Kontak Kami</a>
+
+                @auth()
+                    <a class="nav-item nav-link text-sm text-warning ml-0" style="font-weight: bold" href="/cart">Keranjang</a>
+                    <a class="nav-item nav-link text-sm ml-7 btn btn-outline-warning" style="font-weight: bold" href="/user">Dashboard</a>
+                    <a class="nav-item nav-link text-sm text-warning ml-0" href="/logout">Logout</a>
+                @endauth
+                @guest()
+                    <a class="nav-item nav-link text-sm ml-5" style="font-weight: bold" href="/login">Login</a>
+                @endguest
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="content-wrapper">
+        @yield('content')
+    </div>
+
+    <!-- Footer -->
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <h6>About</h6>
+                    <p class="text-justify">Notaris PPAT Candra Wahyu Nugroho, S.H.,M.Kn</p>
+                    <p class="text-justify">Alamat: Jl. Adi Sucipto No.53, Gatak, Gajahan, Kec. Colomadu, Kabupaten Karanganyar, Jawa Tengah 57174
+                        </p>
+                    <p>Telepon: (0271) 7088153</p>
+                    <a href="https://wa.me/6281393966667?text=Halo%20notaris%20PPAT%20Candra%20Wahyu%20Nugroho" style="font-size: 14px; " class="text-white mb-0" >081393966667 (Whatsapp)</a>
+                </div>
+
+                <div class="col-xs-6 col-md-3">
+                    <h6>Quick Links</h6>
+                    <ul class="footer-links">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/kontak">Contact Us</a></li>
+
+                    </ul>
+                </div>
+            </div>
+            <hr>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-sm-6 col-xs-12">
+                    <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by
+                        <a href="#"></a>.
+                    </p>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <ul class="social-icons">
+                        <li><a class="facebook" href=""><i class="fab fa-facebook"></i></a></li>
+                        <li><a class="dribbble" href=""><i class="fab fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+<!-- Argon Scripts -->
+<!-- Core -->
+<script src="{{asset('assets/vendor/jquery/dist/jquery.min.js')}}"></script>
+<script src="{{asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+<script>
+    feather.replace()
+</script>
+@yield('script')
+
+
+</body>
+
+</html>
---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,38 @@
+@extends('base')
+@section('content')
+
+    <section class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-7">
+                <img src="{{asset('assets/img/slider/kontak.png')}}" style="width: 100%; height: 300px; object-fit: cover">
+            </div>
+
+            <div class="col-5">
+                <p style="font-size: 30px; font-weight: bold" class="mb-3">Notaris PPAT Candra Wahyu Nugroho, S.H.,M.Kn</p>
+                <p style="font-size: 14px; font-weight: bold" class="text-black-50 mb-0" >Jl. Adi Sucipto No.53, Gatak, Gajahan, Kec. Colomadu, Kabupaten Karanganyar, Jawa Tengah 57174</p>
+                <p style="font-size: 14px; font-weight: bold" class="text-black-50 mb-0" >(0271) 7088153</p>
+                <a href="https://wa.me/6281393966667?text=Halo%20notaris%20PPAT%20Candra%20Wahyu%20Nugroho" style="font-size: 14px; font-weight: bold" class="text-black-50 mb-0" >081393966667 (Whatsapp)</a>
+
+
+            </div>
+        </div>
+
+        <div class="d-block bg-gradient-red" style="height: 3px; width: 300px; margin-top: 50px">
+
+        </div>
+
+        <div class="text-left mt-2 mb-3">
+            <h2>Lokasi</h2>
+        </div>
+        <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15821.305661787053!2d110.7615295!3d-7.5393348!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x29b22eac86cc7d1!2sPPAT%20Wahyu%20Nugroho%2C%20SH!5e0!3m2!1sid!2sid!4v1598815502492!5m2!1sid!2sid" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+
+    </section>
+
+@endsection
+
+@section('script')
+
+
+@endsection
