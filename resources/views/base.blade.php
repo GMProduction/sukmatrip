b05e96fa52d357bd586d7c8140f3b4d3d8b0fa67
pradana mahendra <pradanamahendra@gmail.com>
2020-09-08 02:37:10 +0700

    landing page 90% tampilane

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -13,19 +13,21 @@
     <!-- Favicon -->
 {{--    <link rel="icon" href="../assets/img/brand/favicon.png" type="image/png">--}}
 <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet">
     <!-- Icons -->
     <script src="https://unpkg.com/feather-icons"></script>
 </head>
 
-<body style="background-color: #eeeeee">
-<!-- Main content -->
-<div class="main-content" id="panel">
-    <!-- As a link -->
-    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-transparent" style="height: 4em">
-        <div class="container">
-        <a class="navbar-brand text-xl text-primary" href="#" style=" font-weight: bold"><img style="height: 2em" src="{{asset('assets/img/common/logo.png')}}"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+<body>
+
+<nav class="navbar navbar-expand-lg fixed-top navbar-light bg-transparent" style="height: 4em">
+    <div class="container">
+        <a class="navbar-brand text-xl text-primary" href="#" style=" font-weight: bold"><img style="height: 2em"
+                                                                                              src="{{asset('assets/img/common/logo.png')}}"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -36,56 +38,32 @@
                 <a class="nav-item nav-link text-sm ml-5 active" href="/article">ARTICLE</a>
             </div>
         </div>
-        </div>
-    </nav>
-
-
-    <div class="content-wrapper" >
-        @yield('content')
     </div>
+</nav>
 
-    <!-- Footer -->
-    <footer class="site-footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12 col-md-6">
-                    <h6>About</h6>
-                    <p class="text-justify">Notaris PPAT Candra Wahyu Nugroho, S.H.,M.Kn</p>
-                    <p class="text-justify">Alamat: Jl. Adi Sucipto No.53, Gatak, Gajahan, Kec. Colomadu, Kabupaten Karanganyar, Jawa Tengah 57174
-                        </p>
-                    <p>Telepon: (0271) 7088153</p>
-                    <a href="https://wa.me/6281393966667?text=Halo%20notaris%20PPAT%20Candra%20Wahyu%20Nugroho" style="font-size: 14px; " class="text-white mb-0" >081393966667 (Whatsapp)</a>
-                </div>
-
-                <div class="col-xs-6 col-md-3">
-                    <h6>Quick Links</h6>
-                    <ul class="footer-links">
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/kontak">Contact Us</a></li>
-
-                    </ul>
-                </div>
-            </div>
-            <hr>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-sm-6 col-xs-12">
-                    <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by
-                        <a href="#"></a>.
-                    </p>
-                </div>
-
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <ul class="social-icons">
-                        <li><a class="facebook" href=""><i class="fab fa-facebook"></i></a></li>
-                        <li><a class="dribbble" href=""><i class="fab fa-instagram"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
+<div class="content-wrapper">
+    @yield('content')
 </div>
+
+
+<footer class="mt-5 container-fluid" style="height: 15em; position:relative;">
+    <img class="image-as-bg" src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+    <div class="cover-black-all"></div>
+    <div class="d-flex justify-content-center align-items-center h-100 flex-column front">
+        <a class="sukmatrip" style="color: white">STAY CONNECTED</a>
+        <div class="d-flex mt-4">
+            <i class="gen-card-icon mr-2 ml-2 fa fa-facebook-f text-dark"></i>
+            <i class="gen-card-icon mr-2 ml-2 fa fa-facebook-f text-dark"></i>
+        </div>
+        <div class="d-flex mt-4">
+            <a style="color: white" class="mr-2 ml-2">HOME</a>
+            <a style="color: white" class="mr-2 ml-2">CONTACT</a>
+            <a style="color: white" class="mr-2 ml-2">ARTICLE</a>
+            <a style="color: white" class="mr-2 ml-2">GALLERY</a>
+        </div>
+        <p class="text-white mt-3">2019 - All rights reserved to ©Sukmatrip</p>
+    </div>
+</footer>
 <!-- Argon Scripts -->
 <!-- Core -->
 <script src="{{asset('assets/vendor/jquery/dist/jquery.min.js')}}"></script>
--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -1,8 +1,8 @@
 @extends('base')
 @section('content')
 
-{{--    BIG IMAGE--}}
-    <section >
+    {{--    BIG IMAGE--}}
+    <section>
         <div class="gambar-depan" style="background-image: url({{asset('assets/img/foto/sukmatrip1.png')}})">
             <div class="gradien-putih">
             </div>
@@ -11,34 +11,34 @@
                 <p class="sukmatrip">SUKMATRIP</p>
                 <p style="font-size: 3rem; color: white; z-index: 3">CARI TOUR IMPIAN ANDA</p>
                 <div class="container-fluid w-50" style="z-index: 3">
-                    <div class="row " >
-                    <div class="col-lg-4 col-md-12">
-                        <label for="destinasi" class="text-white">Destination</label>
-                        <select class="sukmatrip-form-control" id="destinasi" name="destinasi">
-                            <option class="dropdown-item" value="hotel">All Destination</option>
-                            <option class="dropdown-item"  value="hotel">Bali</option>
-                            <option class="dropdown-item"  value="villa">Lombok</option>
-                        </select>
-                    </div>
+                    <div class="row ">
+                        <div class="col-lg-4 col-md-12">
+                            <label for="destinasi" class="text-white">Destination</label>
+                            <select class="sukmatrip-form-control" id="destinasi" name="destinasi">
+                                <option class="dropdown-item" value="hotel">All Destination</option>
+                                <option class="dropdown-item" value="hotel">Bali</option>
+                                <option class="dropdown-item" value="villa">Lombok</option>
+                            </select>
+                        </div>
 
-                    <div class="col-lg-4 col-md-12">
-                        <label for="tipePenginapan" class="text-white">Category</label>
-                        <select class="sukmatrip-form-control" id="tipePenginapan" name="tipePenginapan">
-                            <option class="dropdown-item" value="hotel">All Category</option>
-                            <option class="dropdown-item" value="hotel">Hotel</option>
-                            <option class="dropdown-item" value="villa">Villa</option>
-                        </select>
-                    </div>
+                        <div class="col-lg-4 col-md-12">
+                            <label for="tipePenginapan" class="text-white">Category</label>
+                            <select class="sukmatrip-form-control" id="tipePenginapan" name="tipePenginapan">
+                                <option class="dropdown-item" value="hotel">All Category</option>
+                                <option class="dropdown-item" value="hotel">Hotel</option>
+                                <option class="dropdown-item" value="villa">Villa</option>
+                            </select>
+                        </div>
 
-                    <div class="col-lg-4 col-md-12">
-                        <label for="durasi" class="text-white">Duration</label>
-                        <select class="sukmatrip-form-control" id="durasi" name="durasi">
-                            <option class="dropdown-item" value="hotel">All Duration</option>
-                            <option class="dropdown-item" value="villa">3 Days 2 Nights</option>
-                            <option class="dropdown-item" value="villa">4 Days 3 Nights</option>
-                            <option class="dropdown-item" value="villa">5 Days 4 Nights</option>
-                        </select>
-                    </div>
+                        <div class="col-lg-4 col-md-12">
+                            <label for="durasi" class="text-white">Duration</label>
+                            <select class="sukmatrip-form-control" id="durasi" name="durasi">
+                                <option class="dropdown-item" value="hotel">All Duration</option>
+                                <option class="dropdown-item" value="villa">3 Days 2 Nights</option>
+                                <option class="dropdown-item" value="villa">4 Days 3 Nights</option>
+                                <option class="dropdown-item" value="villa">5 Days 4 Nights</option>
+                            </select>
+                        </div>
                     </div>
 
                 </div>
@@ -46,21 +46,189 @@
         </div>
     </section>
 
-{{--    OUR PACKAGE--}}
+    {{--    OUR PACKAGE--}}
     <section class="container-fluid">
         <div class="text-center mt-5 mb-5">
             <a class="sukmatrip" style="color: black">SUKMATRIP</a>
 
         </div>
         <p class="text-center f18">It.s Time to Travel</p>
-        <p class="text-center f10 text-black-50">Take a look at the most exclusive & most visited locations in the world - hand-picked just for you.
+        <p class="text-center f10 text-black-50">Take a look at the most exclusive & most visited locations in the world
+            - hand-picked just for you.
             Start traveling the world today!</p>
 
+    </section>
+
+    <section class="container">
         <div class="text-center mt-5 mb-5">
             <a class="sukmatrip" style="color: black">OUR HOTEL</a>
+        </div>
 
+        <div class="row">
+            <div class="col-md-3 col-sm-12">
+                <a class="gen-card-produk">
+                    <img src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+                    <div class="cover-black-bottom"></div>
+                    <div class="content">
+                        <p class="t-accent">PAKET SINI VIE VILLA</p>
+                        <hr style="width: 3em; border-color: var(--accentColor)">
+                        <p class="text-white f18 font-weight-600">Rp. 1.987.600</p>
+                        <p class="text-white f12">3 Day 2 Night</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-sm-12">
+                <a class="gen-card-produk">
+                    <img src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+                    <div class="cover-black-bottom"></div>
+                    <div class="content">
+                        <p class="t-accent">PAKET SINI VIE VILLA</p>
+                        <hr style="width: 3em; border-color: var(--accentColor)">
+                        <p class="text-white f18 font-weight-600">Rp. 1.987.600</p>
+                        <p class="text-white f12">3 Day 2 Night</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-sm-12">
+                <a class="gen-card-produk">
+                    <img src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+                    <div class="cover-black-bottom"></div>
+                    <div class="content">
+                        <p class="t-accent">PAKET SINI VIE VILLA</p>
+                        <hr style="width: 3em; border-color: var(--accentColor)">
+                        <p class="text-white f18 font-weight-600">Rp. 1.987.600</p>
+                        <p class="text-white f12">3 Day 2 Night</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-sm-12">
+                <a class="gen-card-produk">
+                    <img src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+                    <div class="cover-black-bottom"></div>
+                    <div class="content">
+                        <p class="t-accent">PAKET SINI VIE VILLA</p>
+                        <hr style="width: 3em; border-color: var(--accentColor)">
+                        <p class="text-white f18 font-weight-600">Rp. 1.987.600</p>
+                        <p class="text-white f12">3 Day 2 Night</p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3 col-sm-12">
+                <a class="gen-card-produk">
+                    <img src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+                    <div class="cover-black-bottom"></div>
+                    <div class="content">
+                        <p class="t-accent">PAKET SINI VIE VILLA</p>
+                        <hr style="width: 3em; border-color: var(--accentColor)">
+                        <p class="text-white f18 font-weight-600">Rp. 1.987.600</p>
+                        <p class="text-white f12">3 Day 2 Night</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-sm-12">
+                <a class="gen-card-produk">
+                    <img src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+                    <div class="cover-black-bottom"></div>
+                    <div class="content">
+                        <p class="t-accent">PAKET SINI VIE VILLA</p>
+                        <hr style="width: 3em; border-color: var(--accentColor)">
+                        <p class="text-white f18 font-weight-600">Rp. 1.987.600</p>
+                        <p class="text-white f12">3 Day 2 Night</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-sm-12">
+                <a class="gen-card-produk">
+                    <img src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+                    <div class="cover-black-bottom"></div>
+                    <div class="content">
+                        <p class="t-accent">PAKET SINI VIE VILLA</p>
+                        <hr style="width: 3em; border-color: var(--accentColor)">
+                        <p class="text-white f18 font-weight-600">Rp. 1.987.600</p>
+                        <p class="text-white f12">3 Day 2 Night</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-sm-12">
+                <a class="gen-card-produk">
+                    <img src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+                    <div class="cover-black-bottom"></div>
+                    <div class="content">
+                        <p class="t-accent">PAKET SINI VIE VILLA</p>
+                        <hr style="width: 3em; border-color: var(--accentColor)">
+                        <p class="text-white f18 font-weight-600">Rp. 1.987.600</p>
+                        <p class="text-white f12">3 Day 2 Night</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="mt-5 container-fluid" style="height: 30em; position:relative;">
+       <img class="image-as-bg" src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+        <div class="cover-black-all"></div>
+        <div class="d-flex justify-content-center align-items-center h-100 flex-column front">
+            <p class="sukmatrip">SUKMATRIP</p>
+            <p class="f18 font-weight-bold text-white">About us</p>
+            <p class="f10 text-white text-center text-tengah">Passionate about travel and sharing the world's wonders on the leisure travel side. We provide corporate travelers hi-touch services to facilitate their business travel needs. Named each year as one of the "Best Places To Work" in New York.</p>
         </div>
     </section>
+
+    <section class="container mt-5 text-center">
+        <div class="text-center mt-5 mb-5">
+            <a class="sukmatrip" style="color: black">ARTICLE</a>
+        </div>
+        <p class="f14 font-weight-bold">Lastest from the Article</p>
+
+        <div class="row">
+            <div class="col-sm-12 col-md-3">
+                <a class="gen-card-article">
+                    <img src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+                    <p class="judul">INI TEXT</p>
+                    <p class="sumber">Sumbernya</p>
+                </a>
+            </div>
+            <div class="col-sm-12 col-md-3">
+                <a class="gen-card-article">
+                    <img src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+                    <p class="judul">INI TEXT</p>
+                    <p class="sumber">Sumbernya</p>
+                </a>
+            </div>
+            <div class="col-sm-12 col-md-3">
+                <a class="gen-card-article">
+                    <img src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+                    <p class="judul">INI TEXT</p>
+                    <p class="sumber">Sumbernya</p>
+                </a>
+            </div>
+            <div class="col-sm-12 col-md-3">
+                <a class="gen-card-article">
+                    <img src="{{asset('assets/img/foto/sukmatrip1.png')}}">
+                    <p class="judul">INI TEXT</p>
+                    <p class="sumber">Sumbernya</p>
+                </a>
+            </div>
+        </div>
+
+    </section>
+    <hr style="border-color: var(--accentColor)" class="container">
+
+    <section class="container mt-5 text-center">
+        <div class="text-center mt-5 mb-5">
+            <a class="sukmatrip" style="color: black">TESTIMONIALS</a>
+        </div>
+        <p class="f14 font-weight-bold">Our client says:</p>
+        <div class="ulasan mt-3 " style="display: flex; flex-direction: column; align-items: center">
+            <img src="{{asset('assets/img/foto/sukmatrip1.png')}}" style="border-radius: 50%; width: 7em; height: 7em">
+            <p style="color: grey" class="mt-3">Extremelly flexible and easy to use. Code is clean and all files well organized. Great job guys.</p>
+            <p style="color: black; font-weight: bold">JOHN E. PERRY.</p>
+        </div>
+    </section>
+
+
 
 @endsection
 
