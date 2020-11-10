e4c0fa6cfdb1def441818a7a096cf607725c9cf8
portgazdbeel <portgaz77@gmail.com>
2020-11-10 12:00:41 +0700

    commit

--- app/Models/Pakets_tour.php
+++ app/Models/Pakets_tour.php
@@ -18,4 +18,11 @@ class Pakets_tour extends Model
     public function tours(){
         return $this->belongsTo(Image::class,'id_tours');
     }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function tour(){
+        return $this->belongsTo(Tour::class, 'tour_id');
+    }
 }
